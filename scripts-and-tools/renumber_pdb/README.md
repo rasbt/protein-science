e5b5daea04fb2be6d799f2467828366756530301
rasbt <se.raschka@me.com>
2014-10-22 18:05:24 -0400

    atom renumbering

---
+++ scripts-and-tools/renumber_pdb/README.md
@@ -0,0 +1,107 @@
+
+# protein-science - Scripts and Tools
+
+## Renumber PDB Atoms and Residues
+
+
+
+Sebastian Raschka, 10/22/2014  
+**Version 1.0**
+
+<br>
+<br>
+
+<hr>
+I would be happy to hear your comments and suggestions. 
+Please feel free to drop me a note via
+[twitter](https://twitter.com/rasbt), [email](mailto:bluewoodtree@gmail.com), or [google+](https://plus.google.com/+SebastianRaschka).
+<hr>
+
+<br>
+<br>
+
+
+The `renumber.py` script renumbers atoms and/or residues in a PDB file.
+
+### Requirements:
+
+- Python 2.7.x or Python 3.x
+
+<br>
+<br>
+
+### Usage:
+
+run `python renumber.py --help` for the usage information:
+
+<pre>
+usage: renumber.py [-h] [-i INPUT] [-s START] [-a] [-r] [-c]
+
+Renumber residues in a pdb file
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i INPUT, --input INPUT
+                        Input PDB file
+  -s START, --start START
+                        Number of the first residue in the renumbered file (default = 1)
+  -a, --atoms           Renumbers atoms
+  -r, --residues        Renumbers residues
+  -c, --chainreset      Resets the residue renumbering after encountering a new chain.
+  -v, --version         show program's version number and exit
+</pre>
+
+<br>
+<br>
+
+## Example
+
+### Command:
+
+	python renumber.py -i examples/example_1.pdb -a -r  > examples/out_1.pdb
+
+### Input:
+<pre>
+ATOM    774  N   LYS A 105      10.079  34.838  11.847  1.00 29.57           N  
+ATOM    775  CA  LYS A 105      10.147  36.293  12.036  1.00 28.05           C  
+ATOM    776  C   LYS A 105      11.562  36.720  12.391  1.00 27.06           C  
+ATOM    777  O   LYS A 105      12.174  36.150  13.289  1.00 27.14           O  
+ATOM    778  CB  LYS A 105       9.142  36.750  13.090  1.00 27.30           C  
+ATOM    779  CG  LYS A 105       7.712  36.235  12.749  1.00 27.03           C  
+ATOM    780  CD  LYS A 105       6.645  37.016  13.447  1.00 26.83           C  
+ATOM    781  CE  LYS A 105       5.287  36.300  13.367  1.00 24.94           C  
+ATOM    782  NZ  LYS A 105       4.698  36.387  11.996  1.00 23.03           N  
+ATOM    783  N   LEU A 106      12.058  37.734  11.691  1.00 25.68           N  
+ATOM    784  CA  LEU A 106      13.374  38.275  11.962  1.00 25.87           C  
+ATOM    785  C   LEU A 106      13.354  39.143  13.222  1.00 25.22           C  
+ATOM    786  O   LEU A 106      12.314  39.719  13.598  1.00 24.89           O  
+ATOM    787  CB  LEU A 106      13.857  39.118  10.784  1.00 25.38           C  
+ATOM    788  CG  LEU A 106      14.443  38.394   9.564  1.00 27.94           C  
+ATOM    789  CD1 LEU A 106      13.727  37.094   9.184  1.00 27.95           C  
+ATOM    790  CD2 LEU A 106      14.567  39.332   8.388  1.00 26.61           C  
+[...]
+</pre>
+
+### Output:
+<pre>
+ATOM      1  N   LYS A   1      10.079  34.838  11.847  1.00 29.57           N
+ATOM      2  CA  LYS A   1      10.147  36.293  12.036  1.00 28.05           C
+ATOM      3  C   LYS A   1      11.562  36.720  12.391  1.00 27.06           C
+ATOM      4  O   LYS A   1      12.174  36.150  13.289  1.00 27.14           O
+ATOM      5  CB  LYS A   1       9.142  36.750  13.090  1.00 27.30           C
+ATOM      6  CG  LYS A   1       7.712  36.235  12.749  1.00 27.03           C
+ATOM      7  CD  LYS A   1       6.645  37.016  13.447  1.00 26.83           C
+ATOM      8  CE  LYS A   1       5.287  36.300  13.367  1.00 24.94           C
+ATOM      9  NZ  LYS A   1       4.698  36.387  11.996  1.00 23.03           N
+ATOM     10  N   LEU A   2      12.058  37.734  11.691  1.00 25.68           N
+ATOM     11  CA  LEU A   2      13.374  38.275  11.962  1.00 25.87           C
+ATOM     12  C   LEU A   2      13.354  39.143  13.222  1.00 25.22           C
+ATOM     13  O   LEU A   2      12.314  39.719  13.598  1.00 24.89           O
+ATOM     14  CB  LEU A   2      13.857  39.118  10.784  1.00 25.38           C
+ATOM     15  CG  LEU A   2      14.443  38.394   9.564  1.00 27.94           C
+ATOM     16  CD1 LEU A   2      13.727  37.094   9.184  1.00 27.95           C
+ATOM     17  CD2 LEU A   2      14.567  39.332   8.388  1.00 26.61           C
+</pre>
+<br>
+<br>
+	
---
+++ scripts-and-tools/renumber_pdb/renumber.py
@@ -0,0 +1,121 @@
+# Sebastian Raschka 2014
+# Python 3 script to atoms and residues in a PDB file.
+#
+# run
+# ./renumber.py -h
+# for help
+#
+
+class Pdb(object):
+    """ Object that allows operations with protein files in PDB format. """
+
+    def __init__(self, file_cont = [], pdb_code = ""):
+        self.cont = []
+        self.atom = []
+        self.hetatm = []
+        self.fileloc = ""
+        if isinstance(file_cont, list):
+            self.cont = file_cont[:]
+        elif isinstance(file_cont, str):
+            try:
+                with open(file_cont, 'r') as pdb_file:
+                    self.cont = [row.strip() for row in pdb_file.read().split('\n') if row.strip()]
+            except FileNotFoundError as err:
+                print(err)
+
+        if self.cont:
+             self.atom = [row for row in self.cont if row.startswith('ATOM')]
+             self.hetatm = [row for row in self.cont if row.startswith('HETATM')]
+             self.conect = [row for row in self.cont if row.startswith('CONECT')]
+
+    def renumber_atoms(self, start=1):
+        """ Renumbers atoms in a PDB file. """
+        out = list()
+        count = start
+        for row in self.cont:
+            if len(row) > 5:
+                if row.startswith(('ATOM', 'HETATM', 'TER', 'ANISOU')):
+                    num = str(count)
+                    while len(num) < 5:
+                        num = ' ' + num
+                    row = '%s%s%s' %(row[:6], num, row[11:])
+                    count += 1
+            out.append(row)
+        return out
+
+
+    def renumber_residues(self, start=1, reset=False):
+            """ Renumbers residues in a PDB file. """
+            out = list()
+            count = start - 1
+            cur_res = ''
+            for row in self.cont:
+                if len(row) > 25:
+                    if row.startswith(('ATOM', 'HETATM', 'TER', 'ANISOU')):
+                        next_res = row[22:27].strip() # account for letters in res., e.g., '1A'
+                        
+                        if next_res != cur_res:
+                            count += 1
+                            cur_res = next_res
+                        num = str(count)
+                        while len(num) < 3:
+                            num = ' ' + num
+                        new_row = '%s%s' %(row[:23], num)
+                        while len(new_row) < 29:
+                            new_row += ' '
+                        xcoord = row[30:38].strip()
+                        while len(xcoord) < 9:
+                            xcoord = ' ' + xcoord
+                        row = '%s%s%s' %(new_row, xcoord, row[38:])
+                        if row.startswith('TER') and reset:
+                            count = start - 1
+                out.append(row)
+            return out
+
+            
+if __name__ == '__main__':
+    
+    import argparse
+
+
+    parser = argparse.ArgumentParser(
+        description='Renumber residues in a pdb file',
+        formatter_class=argparse.RawTextHelpFormatter
+        )
+
+
+    parser.add_argument('-i', '--input', help='Input PDB file')
+    parser.add_argument('-s', '--start', help='Number of the first residue in the renumbered file (default = 1)')
+    parser.add_argument('-a', '--atoms' ,action='store_true', help='Renumbers atoms')
+    parser.add_argument('-r', '--residues', action='store_true', help='Renumbers residues')
+    parser.add_argument('-c', '--chainreset', action='store_true', help='Resets the residue renumbering after encountering a new chain.')
+    parser.add_argument('-v', '--version', action='version', version='v. 1.0')
+
+    args = parser.parse_args()
+
+    if not args.input:
+        print('{0}\nPlease provide an input file.\n{0}'.format(50* '-'))
+        parser.print_help()
+        quit()
+        
+    if not args.start:
+        start = 1
+    else:
+        start = int(args.start)
+
+    if not args.atoms or not args.residues:
+        print('{0}\nPlease provide at least the --atoms or --residues flag.\n{0}'.format(50* '-'))
+        parser.print_help()
+        quit()
+
+    pdb1 = Pdb(args.input)
+    if args.atoms:
+        pdb1.cont = pdb1.renumber_atoms(start=start)
+    if args.residues:
+        pdb1.cont = pdb1.renumber_residues(start=start, reset=args.chainreset)
+        
+    for line in pdb1.cont:
+        print(line)
+    
+
+
